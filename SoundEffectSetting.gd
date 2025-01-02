862d792af404aca2259089dba5263034f03b7a5e
Exonfang <bobbydesmarais@gmail.com>
2025-01-02 16:07:26 -0500

    static typing, simplifying vars, following gdscript style guidelines

--- SoundEffectSetting.gd
+++ SoundEffectSetting.gd
@@ -1,24 +1,26 @@
+class_name SoundEffect 
 extends Resource
-class_name SoundEffectSettings
-enum SOUND_EFFECT_TYPE{
+
+enum SOUND_EFFECT_TYPE {
 }
 
-@export_range(0, 10) var limit : int = 5
-@export var type : SOUND_EFFECT_TYPE
-@export var sound_effect : AudioStreamMP3
-@export_range(-40, 20) var volume  = 0
-@export_range(0.0, 4.0,.01) var pitch_scale = 1.0
-@export_range(0.0, 1.0,.01) var pitch_randomness = 0.0
+@export_range(0, 10) var limit: int = 5
+@export var type: SOUND_EFFECT_TYPE
+@export var sound_effect: AudioStreamMP3
+@export_range(-40, 20) var volume:float = 0
+@export_range(0.0, 4.0,.01) var pitch_scale: float = 1.0
+@export_range(0.0, 1.0,.01) var pitch_randomness: float = 0.0
+
+var audio_count: int = 0
 
 
-var audio_count = 0
-
-func change_audio_count(amount : int):
+func change_audio_count(amount : int) -> void:
 	audio_count = max(0, audio_count + amount)
-	
+
+
 func has_open_limit() -> bool:
 	return audio_count < limit
-	
-func on_audio_finished():
+
+
+func on_audio_finished() -> void:
 	change_audio_count(-1)
-	
