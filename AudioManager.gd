841e831cc2ae5f0d7dcc15bfede04106f5fe5261
Exonfang <bobbydesmarais@gmail.com>
2025-01-02 16:07:26 -0500

    static typing, simplifying vars, following gdscript style guidelines

--- AudioManager.gd
+++ AudioManager.gd
@@ -1,52 +1,48 @@
-extends Node
+class_name AudioManager
+extends Node2D
 
-var sound_effect_dict = {}
+var sound_effect_dict: Dictionary = {}
 
-@export var sound_effect_settings : Array[SoundEffectSettings]
+@export var sound_effects: Array[SoundEffect]
 
-func _ready():
-	for sound_effect_setting : SoundEffectSettings in sound_effect_settings:
-		sound_effect_dict[sound_effect_setting.type] = sound_effect_setting
-		
-	
-func create_2d_audio_at_location(location, type : SoundEffectSettings.SOUND_EFFECT_TYPE):
+
+func _ready() -> void:
+	for sound_effect: SoundEffect in sound_effects:
+		sound_effect_dict[sound_effect.type] = sound_effect
+
+
+func create_2d_audio_at_location(location, type: SoundEffect.SOUND_EFFECT_TYPE) -> void:
 	if sound_effect_dict.has(type):
-		var sound_effect_setting : SoundEffectSettings = sound_effect_dict[type]
-		if sound_effect_setting.has_open_limit():
-			sound_effect_setting.change_audio_count(1)
-			var new_2D_audio = AudioStreamPlayer2D.new()
+		var sound_effect: SoundEffect = sound_effect_dict[type]
+		if sound_effect.has_open_limit():
+			sound_effect.change_audio_count(1)
+			var new_2D_audio: AudioStreamPlayer2D = AudioStreamPlayer2D.new()
 			add_child(new_2D_audio)
-			
 			new_2D_audio.position = location
-			new_2D_audio.stream = sound_effect_setting.sound_effect
-			new_2D_audio.volume_db = sound_effect_setting.volume
-			new_2D_audio.pitch_scale = sound_effect_setting.pitch_scale
-			new_2D_audio.pitch_scale += Global.rng.randf_range(-sound_effect_setting.pitch_randomness, sound_effect_setting.pitch_randomness )
-			new_2D_audio.finished.connect(sound_effect_setting.on_audio_finished)
+			new_2D_audio.stream = sound_effect.sound_effect
+			new_2D_audio.volume_db = sound_effect.volume
+			new_2D_audio.pitch_scale = sound_effect.pitch_scale
+			new_2D_audio.pitch_scale += Global.rng.randf_range(-sound_effect.pitch_randomness, sound_effect.pitch_randomness )
+			new_2D_audio.finished.connect(sound_effect.on_audio_finished)
 			new_2D_audio.finished.connect(new_2D_audio.queue_free)
-			
 			new_2D_audio.play()
-			
-
 	else:
 		push_error("Audio Manager failed to find setting for type ", type)
-		
-		
-func create_audio(type : SoundEffectSettings.SOUND_EFFECT_TYPE):
-	if sound_effect_dict.has(type):
-		var sound_effect_setting : SoundEffectSettings = sound_effect_dict[type]
-		if sound_effect_setting.has_open_limit():
-			sound_effect_setting.change_audio_count(1)
-			var new_audio = AudioStreamPlayer.new()
-			add_child(new_audio)
 
-			new_audio.stream = sound_effect_setting.sound_effect
-			new_audio.volume_db = sound_effect_setting.volume
-			new_audio.pitch_scale = sound_effect_setting.pitch_scale
-			new_audio.pitch_scale += Global.rng.randf_range(-sound_effect_setting.pitch_randomness, sound_effect_setting.pitch_randomness )
-			new_audio.finished.connect(sound_effect_setting.on_audio_finished)
+
+func create_audio(type: SoundEffect.SOUND_EFFECT_TYPE) -> void:
+	if sound_effect_dict.has(type):
+		var sound_effect: SoundEffect = sound_effect_dict[type]
+		if sound_effect.has_open_limit():
+			sound_effect.change_audio_count(1)
+			var new_audio: AudioStreamPlayer = AudioStreamPlayer.new()
+			add_child(new_audio)
+			new_audio.stream = sound_effect.sound_effect
+			new_audio.volume_db = sound_effect.volume
+			new_audio.pitch_scale = sound_effect.pitch_scale
+			new_audio.pitch_scale += Global.rng.randf_range(-sound_effect.pitch_randomness, sound_effect.pitch_randomness )
+			new_audio.finished.connect(sound_effect.on_audio_finished)
 			new_audio.finished.connect(new_audio.queue_free)
-			
 			new_audio.play()
 	else:
 		push_error("Audio Manager failed to find setting for type ", type)
